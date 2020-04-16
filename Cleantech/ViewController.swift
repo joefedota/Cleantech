8f634995710ba9427965a26b09e7b47ec2879b6c
Joe Fedota <joefedota@berkeley.edu>
2020-04-15 23:32:58 -0700

    Initial Commit

---
+++ Cleantech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cleantech
+//
+//  Created by Joseph Fedota on 4/15/20.
+//  Copyright © 2020 23O2 Automatic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleantechTests/CleantechTests.swift
@@ -0,0 +1,34 @@
+//
+//  CleantechTests.swift
+//  CleantechTests
+//
+//  Created by Joseph Fedota on 4/15/20.
+//  Copyright © 2020 23O2 Automatic. All rights reserved.
+//
+
+import XCTest
+@testable import Cleantech
+
+class CleantechTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
